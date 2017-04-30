649ebc53ce67dd0079f3e814af0e5d94ebcdba53
Juan Manuel Perales <Juan Manuel Perales>
2017-04-30 21:33:20 +0200

    my first commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,40 @@
+
+import { EventEmitter, Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe(
+            'Tarta de Manzana', 
+            'Una deliciosa merienda!', 
+            'http://www.rebanando.com/uploads/media/alejandrobodas2b.jpg?1395586243',
+            [
+                new Ingredient('Manzana', 2),
+                new Ingredient('Leche', 1)
+            ]),
+        new Recipe(
+            'Tiramissu', 
+            'Un postre espectacular!',
+            'https://labombacha.com/wp-content/uploads/2016/04/Tiramisu.jpg',
+            [
+                new Ingredient('Huevos', 2),
+                new Ingredient('Queso', 1)
+            ])
+    ];
+
+    constructor(private slService: ShoppingListService){}
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+    }
+}
